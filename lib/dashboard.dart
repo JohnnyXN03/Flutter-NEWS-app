de6c417b818e3fa16725182055e8a26032eeda79
JohnnyXN03 <kjjatinnirban@gmail.com>
2020-06-01 02:29:10 +0530

    updates

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:newsx/descr.dart';
@@ -16,7 +16,6 @@ class _IndiaPageState extends State<IndiaPage> {
   List topnews = [];
 
   Future<List<dynamic>> fetchData() async {
-    Random();
     http.Response response = await http.get(
         'http://newsapi.org/v2/top-headlines?country=in&&apiKey=17141852308c45f9813b14ce49f70996');
     setState(() {
@@ -25,9 +24,8 @@ class _IndiaPageState extends State<IndiaPage> {
   }
 
   Future<List<dynamic>> fetchDatatop() async {
-    Random();
     http.Response response = await http.get(
-        'http://newsapi.org/v2/top-headlines?country=in&apiKey=17141852308c45f9813b14ce49f70996');
+        'http://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=17141852308c45f9813b14ce49f70996');
     setState(() {
       topnews = json.decode(response.body)['articles'];
     });
@@ -59,70 +57,61 @@ class _IndiaPageState extends State<IndiaPage> {
                 shrinkWrap: true,
                 itemCount: topnews.length,
                 itemBuilder: (context, index) {
-                  return topnews[index] == null
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : InkWell(
-                          onTap: () {
-                            var url = topnews[index]['url'];
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        Descr(url)));
-                          },
-                          hoverColor: Colors.transparent,
-                          child: Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 3),
-                            padding: EdgeInsets.all(5),
-                            child: SizedBox(
-                              height: 300,
-                              width: 400,
-                              child: Card(
-                                margin: EdgeInsets.all(10),
-                                elevation: 5,
-                                child: Stack(
-                                  fit: StackFit.passthrough,
-                                  children: <Widget>[
-                                    Container(
-                                      child: topnews[index]['urlToImage'] ==
-                                              null
-                                          ? Image.asset('assets/newsback.jpg')
-                                          : Image.network(
-                                              topnews[index]['urlToImage'],
-                                              alignment: Alignment.center,
-                                              fit: BoxFit.fill,
-                                            ),
-                                    ),
-                                    SizedBox(
-                                      height: 50,
-                                      width: 400,
-                                      child: Container(
-                                        alignment: Alignment.bottomCenter,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white10,
-                                        ),
-                                        padding: EdgeInsets.all(20),
-                                        child: Text(
-                                          '' +
-                                              topnews[index]['title']
-                                                  .toString(),
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontFamily: 'OpenSans',
-                                            fontSize: 20,
-                                          ),
-                                        ),
+                  return InkWell(
+                    onTap: () {
+                      var url = topnews[index]['url'];
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Descr(url)));
+                    },
+                    hoverColor: Colors.transparent,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 3),
+                      padding: EdgeInsets.all(5),
+                      child: SizedBox(
+                        height: 300,
+                        width: 400,
+                        child: Card(
+                          margin: EdgeInsets.all(10),
+                          elevation: 5,
+                          child: Stack(
+                            fit: StackFit.passthrough,
+                            children: <Widget>[
+                              Container(
+                                child: topnews[index]['urlToImage'] == null
+                                    ? Image.asset('assets/newsback.jpg')
+                                    : Image.network(
+                                        topnews[index]['urlToImage'],
+                                        alignment: Alignment.center,
+                                        fit: BoxFit.fill,
                                       ),
+                              ),
+                              SizedBox(
+                                height: 50,
+                                width: 400,
+                                child: Container(
+                                  alignment: Alignment.bottomCenter,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white10,
+                                  ),
+                                  padding: EdgeInsets.all(20),
+                                  child: Text(
+                                    '' + topnews[index]['title'].toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'OpenSans',
+                                      fontSize: 20,
                                     ),
-                                  ],
+                                  ),
                                 ),
                               ),
-                            ),
+                            ],
                           ),
-                        );
+                        ),
+                      ),
+                    ),
+                  );
                 }),
           ),
           Container(
@@ -151,71 +140,65 @@ class _IndiaPageState extends State<IndiaPage> {
           Expanded(
             child: ListView.builder(
               itemBuilder: (context, index) {
-                return india[index] == null
-                    ? Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : Container(
-                        height: 100,
-                        child: InkWell(
-                          onTap: () {
-                            var url1 = india[index]['url'];
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        Descr(url1)));
-                          },
-                          child: Card(
-                            color: Colors.white,
-                            elevation: 0.2,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                SizedBox(
-                                  height: 80,
-                                  width: 110,
-                                  child: Container(
-                                    padding: EdgeInsets.all(8),
-                                    child: india[index]['urlToImage'] == null
-                                        ? Image.asset('assets/newsback.jpg')
-                                        : Image.network(
-                                            india[index]['urlToImage'],
-                                            fit: BoxFit.fill,
-                                          ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 250,
-                                  height: 100,
-                                  child: Container(
-                                    padding: EdgeInsets.all(12),
-                                    child: Text(
-                                      '' + india[index]['title'],
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w600,
-                                      ),
+                return Container(
+                  height: 100,
+                  child: InkWell(
+                    onTap: () {
+                      var url1 = india[index]['url'];
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Descr(url1)));
+                    },
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 0.2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 80,
+                            width: 110,
+                            child: Container(
+                              padding: EdgeInsets.all(8),
+                              child: india[index]['urlToImage'] == null
+                                  ? Image.asset('assets/newsback.jpg')
+                                  : Image.network(
+                                      india[index]['urlToImage'],
+                                      fit: BoxFit.fill,
                                     ),
-                                  ),
-                                ),
-                                IconButton(
-                                    icon: Icon(
-                                      Icons.share,
-                                      size: 20,
-                                    ),
-                                    onPressed: () async {
-                                      FlutterShareMe().shareToWhatsApp(
-                                          msg:
-                                              'Shared Through Tanwave News App.  ' +
-                                                  india[index]['url']);
-                                    })
-                              ],
                             ),
                           ),
-                        ),
-                      );
+                          SizedBox(
+                            width: 250,
+                            height: 100,
+                            child: Container(
+                              padding: EdgeInsets.all(12),
+                              child: Text(
+                                '' + india[index]['title'],
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                              icon: Icon(
+                                Icons.share,
+                                size: 20,
+                              ),
+                              onPressed: () async {
+                                FlutterShareMe().shareToWhatsApp(
+                                    msg: 'Shared Through Tanwave News App.  ' +
+                                        india[index]['url']);
+                              })
+                        ],
+                      ),
+                    ),
+                  ),
+                );
               },
               itemCount: india.length == null ? 0 : india.length,
             ),
